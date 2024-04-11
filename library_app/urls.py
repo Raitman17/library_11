745d8331d9f1fff34a6875f532047e28e90dfa89
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-04-11 14:26:25 +0300

    all base views authors, genres, books

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='homepage'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path('book/', views.view_book, name='book'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/', views.view_author, name='author'),
+    path('genres/', views.GenreListView.as_view(), name='genres'),
+    path('genre/', views.view_genre, name='genre'),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -16,17 +16,40 @@ def home_page(request):
         }
     )
 
-class BookListView(ListView):
-    model = Book
-    template_name = 'catalog/books.html'
-    paginate_by = 10
-    context_object_name = 'books'
+def create_listview(model_class, plural_name, template):
+    class CustomListView(ListView):
+        model = model_class
+        template_name = template
+        paginate_by = 10
+        context_object_name = plural_name
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        instances = Book.objects.all()
-        paginator = Paginator(instances, 10)
-        page = self.request.GET.get('page')
-        page_obj = paginator.get_page(page)
-        context['books_list'] = page_obj
-        return context
+        def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+            context = super().get_context_data(**kwargs)
+            instances = model_class.objects.all()
+            paginator = Paginator(instances, 10)
+            page = self.request.GET.get('page')
+            page_obj = paginator.get_page(page)
+            context[f'{plural_name}_list'] = page_obj
+            return context
+    return CustomListView
+
+BookListView = create_listview(Book, 'books', 'catalog/books.html')
+AuthorListView = create_listview(Author, 'authors', 'catalog/authors.html')
+GenreListView = create_listview(Genre, 'genres', 'catalog/genres.html')
+
+def create_view(model_class, context_name, template):
+    def view(request):
+        id_ = request.GET.get('id', None)
+        target = model_class.objects.get(id=id_) if id_ else None
+        return render(
+            request,
+            template,
+            {
+                context_name: target,
+            }
+        )
+    return view
+
+view_book = create_view(Book, 'book', 'entities/book.html')
+view_author = create_view(Author, 'author', 'entities/author.html')
+view_genre = create_view(Genre, 'genre', 'entities/genre.html')
