4dfa264d950a97c96f894b2f621946aa8ddbb1a3
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-04-27 15:47:49 +0300

    workflow for github tests

--- library/settings.py
+++ library/settings.py
@@ -7,12 +7,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7-2e1*m9%25@xjzi3*hlh4&_$3i5%1tmk3lf*bc6^sert5ga4'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
