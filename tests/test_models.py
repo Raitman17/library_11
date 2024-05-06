c8b889e36a0ce3f3f28925070b38a1af9a8febe3
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-05-06 19:07:17 +0300

    test validators, str models methods and add funds

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from datetime import date, datetime, timezone, timedelta
 from django.contrib.auth.models import User
 
-from library_app.models import Book, Genre, Author, Client
+from library_app.models import Book, Genre, Author, Client, check_created, check_modified, check_positive, validate_year
 
 def create_model_test(model_class, valid_attrs: dict, bunch_of_invalid_attrs: Iterable = None):
     class ModelTest(TestCase):
@@ -34,11 +34,54 @@ GenreModelTest = create_model_test(Genre, {'name': 'ABC'})
 
 class ClientTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username='abc', password='abc')
+        self.user = User.objects.create(username='abc', first_name='abc', last_name='abc', password='abc')
 
     def test_invalid(self):
         with self.assertRaises(ValidationError):
             Client.objects.create(user=self.user, money=-1)
 
-    def test_valid(self):
-        Client.objects.create(user=self.user)
+    def test_create_and_str(self):
+        self.assertEqual(str(Client.objects.create(user=self.user)), 'abc (abc abc)')
+
+
+def create_str_test(model_class, attrs, expected_str):
+    def test(self):
+        self.assertEqual(str(model_class.objects.create(**attrs)), expected_str)
+    return test
+
+str_test_data = (
+    (Book, {'title': 'ABC', 'type': 'book', 'volume': 1}, 'ABC, book, 1 pages'),
+    (Genre, {'name': 'ABC', 'description': 'ABC'}, 'ABC'),
+    (Author, {'full_name': 'ABC'}, 'ABC'),
+)
+
+str_methods = {f'test_{args[0].__name__}': create_str_test(*args) for args in str_test_data}
+StrTest  = type('StrTest', (TestCase,), str_methods)
+
+PAST = datetime(datetime.today().year-1, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc)
+FUTURE = datetime(datetime.today().year+1, 1, 1, 1, 1, 1, 1, tzinfo=timezone.utc)
+
+validators_pass = (
+    (check_positive, 1),
+    (check_created, PAST),
+    (check_modified, PAST),
+    (validate_year, PAST.year),
+)
+
+validators_fail = (
+    (check_positive, -1),
+    (check_created, FUTURE),
+    (check_modified, FUTURE),
+    (validate_year, FUTURE.year),
+)
+
+def create_val_test(validator, value, valid=True):
+    def test(self):
+        with self.assertRaises(ValidationError):
+            validator(value)
+    return lambda _ : validator(value) if valid else test
+
+invalid_methods = {f'test_inval_{args[0].__name__}': create_val_test(*args, valid=False) for args in validators_fail}
+valid_methods = {f'test_val_{args[0].__name__}': create_val_test(*args) for args in validators_pass}
+
+ValidatorsTest = type('ValidatorsTest', (TestCase,), invalid_methods | valid_methods)
